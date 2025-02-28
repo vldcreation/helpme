eddfa38a4198af87e1d7cc23d09a4415a44b42e2
Vicktor Desronyclear <vldcreation21@gmail.com>
2025-02-28 10:52:46 +0700

    ref: cleaning code

--- main.go
+++ main.go
@@ -4,17 +4,15 @@ import (
 	"vldcreation/github.com/helpme/cmd"
 )
 
+const (
+	RED_COLOR    = "\033[0;31m"
+	BLUE_COLOR   = "\033[0;34m"
+	YELLOW_COLOR = "\033[0;33m"
+	RESET_COLOR  = "\033[0m"
+)
+
 func main() {
 	if err := cmd.NewApp().Execute(); err != nil {
-		panic(err)
+		println(string(RED_COLOR), err.Error(), string(RESET_COLOR))
 	}
-
-	// interopOb := interop.NewInterop("javascript", "interop/javascript/ai.js")
-	// output, err := interopOb.Run()
-	// if err != nil {
-	// 	panic(err)
-	// }
-
-	// println("Output:")
-	// println(output.String())
 }
