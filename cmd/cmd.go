9779ee2dc76425518ac2b8811f03bfb0b1d92d09
Vicktor Desrony <vldcreation21@gmail.com>
2025-03-21 13:28:54 +0700

    refactor(profiling): move profiling from main to cmd

--- cmd/cmd.go
+++ cmd/cmd.go
@@ -6,6 +6,8 @@ import (
 	"runtime/debug"
 
 	"github.com/spf13/cobra"
+	"github.com/vldcreation/helpme/pkg/profiler"
+	"github.com/vldcreation/helpme/util"
 )
 
 type CMD interface {
@@ -14,6 +16,7 @@ type CMD interface {
 
 type App struct {
 	root *cobra.Command
+	prof *profiler.Profiler
 }
 
 var (
@@ -34,16 +37,50 @@ func printVersion() string {
 }
 
 func NewApp() CMD {
+	app := &App{}
 	rootCmd := &cobra.Command{
 		Use:   "helpme",
 		Short: "Helpme CLI tool",
 		Long:  `A CLI tool for finding and setting up development templates`,
+		PersistentPreRun: func(cmd *cobra.Command, args []string) {
+			cpuProfile, _ := cmd.Flags().GetBool("cpuprofile")
+			memProfile, _ := cmd.Flags().GetBool("memprofile")
+
+			if cpuProfile || memProfile {
+				prof, err := profiler.New("./profiles")
+				if err != nil {
+					util.PrintlnRed(err.Error())
+					return
+				}
+				app.prof = prof
+
+				if cpuProfile {
+					if err := prof.StartCPUProfile(); err != nil {
+						util.PrintlnRed(err.Error())
+						return
+					}
+				}
+			}
+		},
+		PersistentPostRun: func(cmd *cobra.Command, args []string) {
+			if app.prof != nil {
+				app.prof.StopCPUProfile()
+				memProfile, _ := cmd.Flags().GetBool("memprofile")
+				if memProfile {
+					if err := app.prof.WriteHeapProfile(); err != nil {
+						util.PrintlnRed(err.Error())
+					}
+				}
+			}
+		},
 		Run: func(cmd *cobra.Command, args []string) {
 			slog.Info("Root command executed successfully")
 		},
 	}
 
 	rootCmd.Version = printVersion()
+	rootCmd.PersistentFlags().BoolP("memprofile", "m", false, "enable memory profiling")
+	rootCmd.PersistentFlags().BoolP("cpuprofile", "c", false, "enable cpu profiling")
 
 	// Register subcommands
 	rootCmd.AddCommand(
@@ -55,9 +92,8 @@ func NewApp() CMD {
 		NewEncodeCommand().Command(),
 	)
 
-	return &App{
-		root: rootCmd,
-	}
+	app.root = rootCmd
+	return app
 }
 
 func (app *App) Execute() error {
